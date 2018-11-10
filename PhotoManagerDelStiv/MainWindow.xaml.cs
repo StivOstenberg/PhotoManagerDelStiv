3dc528cba57080f930371d9734a3b86f6d34a7eb
stivostenberg <megastiv@stiv.com>
2018-11-10 14:11:55 +0900

    Working with basic functions

--- PhotoManagerDelStiv/MainWindow.xaml.cs
+++ PhotoManagerDelStiv/MainWindow.xaml.cs
@@ -42,6 +42,7 @@ namespace PhotoManagerDelStiv
         public string Move2Root = @"D:\Camera";
         public static List<string> PicFiles = new List<string>();
         public static List<string> skipFiles = new List<string>();
+        public int PicIndex = 0;
 
         public string Trash = @"D:\Basura";
 
@@ -53,7 +54,7 @@ namespace PhotoManagerDelStiv
             // Check for source directory and settings here
             getFilenames(SourceRoot);
 
-            var diditwork = LoadPicture(PicFiles[0]);
+            var diditwork = LoadPicture(PicFiles[PicIndex]);
 
         }
 
@@ -116,160 +117,10 @@ namespace PhotoManagerDelStiv
             }
         }
 
-        public bool LoadPicture(string picturepath)
-        {
-            GPS_button.Content = "No GPS Coordinates Found!";
-            GPS_button.IsEnabled = false;
-            // Get metadata from original image
-            //Get der metadata
-            Bitmap imagemeta = new Bitmap(picturepath);
-            System.Drawing.Imaging.PropertyItem[] propItems = imagemeta.PropertyItems;
-            string Coordinates = "";
-            string Altitude = "";
-
-            //Get the image for the display without locking file.
-            BitmapImage image = new BitmapImage();
-            image.BeginInit();
-            image.CacheOption = BitmapCacheOption.OnLoad;
-            image.UriSource = new Uri(picturepath);
-            image.EndInit();
-            FilenameLable.Content = picturepath;
-            CurrentPicture.Source = image;
-            CurrentPicture.UpdateLayout();
-            //Metadata Extractor
-            var meps = ImageMetadataReader.ReadMetadata(picturepath);
-            
-            IEnumerable<MetadataExtractor.Directory> directories = ImageMetadataReader.ReadMetadata(picturepath);
-
-
-            try {
-                var GPS = directories.OfType<MetadataExtractor.Formats.Exif.GpsDirectory>().FirstOrDefault();
-                var tags = GPS.Tags;
-                Coordinates = "GPS Coordinates: " + tags[1].Description + " " + tags[2].Description + " , " + tags[3].Description +" "+ tags[4].Description;
-                Coordinates=Coordinates.Replace(@"\", "");
-                Altitude = tags[6].Description +" "+ tags[5].Description;
-                string myLat = tags[2].Description.Replace(@"\", "") + tags[1].Description;
-                string myLong = tags[4].Description.Replace(@"\", "") + tags[3].Description;
-                myGPS = myLat.Replace(" ", string.Empty) + " " + myLong.Replace(" ", string.Empty);
-
-                Coordinate C = new Coordinate();
-                Coordinate.TryParse(myGPS,out C);
-                myGPS = C.Latitude.DecimalDegree + " " + C.Longitude.DecimalDegree;
-
-                
-
-                GPS_button.Content = Coordinates;
-                GPS_button.IsEnabled = true;
-                
-            }
-            catch
-            {
-                //No GPS coordinates found!
-            }
-
-            JpegBitmapDecoder jpgDecoder = new JpegBitmapDecoder(new MemoryStream(File.ReadAllBytes(picturepath)), BitmapCreateOptions.PreservePixelFormat, BitmapCacheOption.OnLoad);
-            BitmapMetadata myBitmapMetadata = new BitmapMetadata("jpg");
-            JpegBitmapEncoder jpgEncoder = new JpegBitmapEncoder();
-            BitmapSource bitmapSource;
-
-            if (jpgDecoder != null)
-            {
-                bitmapSource = jpgDecoder.Frames[0];
-                var Meat = bitmapSource.Metadata;  //Returns Image Metadata, not Bitmap metadata.
-                myBitmapMetadata = (BitmapMetadata)Meat;
-
-
-            }
-            jpgEncoder = null;
-            jpgDecoder = null;
-            myBitmapMetadata = null;
-            return true;
-
-        }
 
 
 
-        private void RotateClock(string file)
-        {
-            try {
-                //Get der metadata
-                Bitmap imagemeta = new Bitmap(file);
-                System.Drawing.Imaging.PropertyItem[] propItems = imagemeta.PropertyItems;
 
-                BitmapImage image = new BitmapImage();
-                image.BeginInit();
-                image.CacheOption = BitmapCacheOption.OnLoad;
-                image.UriSource = new Uri(file);
-                image.EndInit();
-
-                Bitmap bitmap1 = BitmapImage2Bitmap(image);
-                bitmap1 = (Bitmap)Bitmap.FromFile(PicFiles[0]);
-
-                //Based on argument, select operation
-
-                bitmap1.RotateFlip(RotateFlipType.Rotate90FlipNone);
-
-
-                string testfile = @"D:\Pictures2Process\Out.Jpg";
-
-
-                bitmap1.Save(testfile);
-                //Add Metadata to new image
-                imagemeta = new Bitmap(testfile);
-
-                foreach (var aprop in propItems)
-                {
-                    imagemeta.SetPropertyItem(aprop);
-                }
-                imagemeta.Save(testfile);
-                LoadPicture(PicFiles[0]);
-
-
-
-            }
-            catch(Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
-        }
-
-        private void RotateCounterClock(string file)
-        {
-            try
-            {
-                BitmapImage image = new BitmapImage();
-                image.BeginInit();
-                image.CacheOption = BitmapCacheOption.OnLoad;
-                image.UriSource = new Uri(file);
-                image.EndInit();
-
-                Bitmap bitmap1 = BitmapImage2Bitmap(image);
-                bitmap1 = (Bitmap)Bitmap.FromFile(PicFiles[0]);
-                bitmap1.RotateFlip(RotateFlipType.Rotate270FlipNone);
-                bitmap1.Save(file);
-                LoadPicture(PicFiles[0]);
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
-        }
-
-
-        private Bitmap BitmapImage2Bitmap(BitmapImage bitmapImage)
-        {
-            // BitmapImage bitmapImage = new BitmapImage(new Uri("../Images/test.png", UriKind.Relative));
-
-            using (MemoryStream outStream = new MemoryStream())
-            {
-                BitmapEncoder enc = new BmpBitmapEncoder();
-                enc.Frames.Add(BitmapFrame.Create(bitmapImage));
-                enc.Save(outStream);
-                System.Drawing.Bitmap bitmap = new System.Drawing.Bitmap(outStream);
-
-                return new Bitmap(bitmap);
-            }
-        }
 
 
 
@@ -282,7 +133,7 @@ namespace PhotoManagerDelStiv
         /// </summary>
         /// <param name="file">The name of the file to modify</param>
         /// <param name="operation">RotateCCW, RotateCW, Resize50, Long1024</param>
-        public void ModJPEG(string filename, string operation)
+        public bool ModJPEG(string filename, string operation)
         {
             byte[] photoBytes = File.ReadAllBytes(filename);   
             // Format is automatically detected though can be changed.
@@ -298,7 +149,7 @@ namespace PhotoManagerDelStiv
                         switch (operation)
                         {
                             case "RotateCW":
-                                     imageFactory.Load(inStream).Format(format).AutoRotate().Rotate(90).Save(outStream);
+                                     imageFactory.Load(inStream).Format(format).Rotate(90).Save(outStream);
                                 break;
                             case "RotateCCW":
                                 imageFactory.Load(inStream).Format(format).AutoRotate().Rotate(270).Save(outStream);
@@ -316,15 +167,111 @@ namespace PhotoManagerDelStiv
                     }
                     inStream.Close();
                     inStream.Dispose();
-                    
-                    File.WriteAllBytes(filename, outStream.ToArray());
+                    try {
+                        File.WriteAllBytes(filename, outStream.ToArray());
+                        OpStat.Content = "Write succeeded!";
+                        OpStat.Tag = "Modified " + filename;
+                        OpStat.ToolTip = "Modified " + filename;
+                    }
+                    catch(Exception ex)
+                    {
+                        OpStat.Content = "Write failed!";
+                        OpStat.Tag = ex.Message;
+                        OpStat.ToolTip = ex.Message;
+                        return false;
+                    }
                     outStream.Close();
                     outStream.Dispose();
-
+                    return true;
                     
                 }
             }
         }
+
+        public bool LoadPicture(string picturepath)
+        {
+            FileStream testfile;
+            try {
+                testfile = File.OpenWrite(picturepath);
+                SetButtonsState(true);
+                testfile.Close();
+            }
+            catch
+            {
+                SetButtonsState(false);
+                
+            }
+
+
+            GPS_button.Content = "GPS Data";
+            GPS_button.IsEnabled = false;
+            // Get metadata from original image
+            //Get der metadata
+
+            string Coordinates = "";
+            string Altitude = "";
+
+            //Get the image for the display without locking file.
+            
+
+            BitmapImage image = new BitmapImage();
+            image.BeginInit();
+            image.CacheOption = BitmapCacheOption.OnLoad;
+            image.UriSource = new Uri(picturepath);
+            image.EndInit();
+            
+
+
+            FilenameLable.Content = PicIndex+1 +" of " + PicFiles.Count + " - " + picturepath;
+
+
+            //Metadata Extractor
+            var meps = ImageMetadataReader.ReadMetadata(picturepath);
+            IEnumerable<MetadataExtractor.Directory> directories = ImageMetadataReader.ReadMetadata(picturepath);
+
+
+            try
+            {
+                var GPS = directories.OfType<MetadataExtractor.Formats.Exif.GpsDirectory>().FirstOrDefault();
+                var tags = GPS.Tags;
+                Coordinates = "GPS Coordinates: " + tags[1].Description + " " + tags[2].Description + " , " + tags[3].Description + " " + tags[4].Description;
+                Coordinates = Coordinates.Replace(@"\", "");
+                Altitude = tags[6].Description + " above " + tags[5].Description;
+                string myLat = tags[2].Description.Replace(@"\", "") + tags[1].Description;
+                string myLong = tags[4].Description.Replace(@"\", "") + tags[3].Description;
+                myGPS = myLat.Replace(" ", string.Empty) + " " + myLong.Replace(" ", string.Empty);
+
+                Coordinate C = new Coordinate();
+                Coordinate.TryParse(myGPS, out C);
+                myGPS = C.Latitude.DecimalDegree + " " + C.Longitude.DecimalDegree;
+
+
+
+                GPS_button.Content = Coordinates + "     " + Altitude;
+                GPS_button.IsEnabled = true;
+
+            }
+            catch
+            {
+                GPS_button.Content = "No GPS Coordinates Found!";
+            }
+
+
+            CurrentPicture.Source = image.Clone();
+            CurrentPicture.UpdateLayout();
+            MainContainer.UpdateLayout();
+            
+            return true;
+            
+
+        }
+
+        public void SetButtonsState(bool state)
+        {
+            RotateClockButton.IsEnabled = state;
+            rotateCCWbutton.IsEnabled = state;
+        }
+        #endregion Functions
 
         ///UI Action Events
 
@@ -332,27 +279,49 @@ namespace PhotoManagerDelStiv
         {
             
             try {
-                PicFiles.RemoveAt(0);
-                LoadPicture(PicFiles[0]);
+                PicIndex++;
+                LoadPicture(PicFiles[PicIndex]);
     }
             catch
             {
+                PicIndex--;
+                LoadPicture(PicFiles[PicIndex]);
+            }
 
+        }
+
+        private void BackButton_Click(object sender, RoutedEventArgs e)
+        {
+
+            try
+            {
+                PicIndex--;
+                LoadPicture(PicFiles[PicIndex]);
+            }
+            catch
+            {
+                PicIndex++;
+                LoadPicture(PicFiles[PicIndex]);
             }
 
         }
 
         private void RotateClockButton_Click(object sender, RoutedEventArgs e)
         {
-            ModJPEG(PicFiles[0],"RotateCW");
-            LoadPicture(PicFiles[0]);
+            CurrentPicture.Source = null;
+
+            
+            var OK = ModJPEG(PicFiles[PicIndex],"RotateCW");
+            
+            LoadPicture(PicFiles[PicIndex]);
 
         }
 
         private void rotateCCWbutton_Click(object sender, RoutedEventArgs e)
         {
-            ModJPEG(PicFiles[0], "RotateCCW");
-            LoadPicture(PicFiles[0]);
+            CurrentPicture.Source = null;
+            var OK=ModJPEG(PicFiles[PicIndex], "RotateCCW");
+            if (OK) LoadPicture(PicFiles[PicIndex]);
         }
 
         private void button_Click(object sender, RoutedEventArgs e)
@@ -400,6 +369,20 @@ namespace PhotoManagerDelStiv
             System.Diagnostics.Process.Start(url);
 
         }
+
+        private void FirstButton_Click(object sender, RoutedEventArgs e)
+        {
+            PicIndex = 0;
+            LoadPicture(PicFiles[PicIndex]);
+        }
+
+        private void lastbutton_Click(object sender, RoutedEventArgs e)
+        {
+            PicIndex = PicFiles.Count-1;
+            LoadPicture(PicFiles[PicIndex]);
+        }
+
+
     }
 
 
